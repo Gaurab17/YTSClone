3c00052d9bf8f16874469afd7ecf8e3fab9c6018
Gaurab17 <gaurabstha001@gmail.com>
2023-03-06 13:01:09 +0545

    Latest Movie Section added

--- src/components/Molecules/LatestMovies/index.jsx
+++ src/components/Molecules/LatestMovies/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import MovieTemplate from "../../Atom/MovieTemplate/MovieTemplate";
+import "./LatestMovies.css";
 
-const LatestMovies = () => {
+const LatestMovies = ({ movieDetails: latestMovies }) => {
     return (
-        <div>LatestMovies</div>
-    )
+        <section className="latest-movies">
+            <div className="latest-movies_container">
+                <div className="latest-movies-header-section">
+                    <h1 className="latest-movies_title">Latest YIFY Movies Torrents</h1>
+                    <a href="#" className="latest-movies_browse-more">
+                        Browse All
+                    </a>
+                </div>
+                <ul className="latest-movies_movie">
+                    {latestMovies.map((movie) => {
+                        return <MovieTemplate key={movie.id} movieDetails={movie} />;
+                    })}
+                </ul>
+            </div>
+        </section>
+    );
 }
 
-export default LatestMovies
+export default LatestMovies;
