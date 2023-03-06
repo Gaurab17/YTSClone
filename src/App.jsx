171851fb7ea5d6696c2fdb9c0c80b47cba6314ef
Gaurab17 <gaurabstha001@gmail.com>
2023-03-06 13:00:02 +0545

    Outlet Configured for routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './layout/Layout'
 import LandingPage from './screens/landingPage'
+import MovieDownload from "./components/Molecules/MovieDownloads"
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="" element={<Layout />}>
         <Route
-          path=""
+          path="/"
           element={<LandingPage />}
         />
       </Route>
